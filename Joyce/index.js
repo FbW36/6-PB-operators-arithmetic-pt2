0f115212adf9986daaca96e2aebedf4ded41f2eb
Joyce <puch1996@gmail.com>
2020-10-06 14:47:24 +0200

    modified

---
+++ Joyce/index.js
@@ -0,0 +1,26 @@
+// Q1
+let revenue = 6450;
+let ticketCost = 15;
+let ticketsSold = revenue / ticketCost;
+console.log(ticketsSold);
+
+// Q2
+let weekIncome = 500;
+let yearIncome = 500 * 52;
+console.log(yearIncome);
+
+// Q3
+let per = (17 / 30) * 100;
+console.log(per + "%");
+
+// Q4
+let perimeter1 = 4.75 * 4;
+console.log(perimeter1);
+
+// Q5
+let perimeter2 = 5 + 6 + 7;
+console.log(perimeter2);
+
+// Q6
+let square = 5 * 5;
+console.log(square);
