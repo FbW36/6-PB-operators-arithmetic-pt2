039933de03c3a001b63f4908d6b3857e3418ede2
Chrissi <cmette.online@googlemail.com>
2020-10-06 16:47:10 +0200

    Solve excercise

---
+++ christin/index.js
@@ -0,0 +1,44 @@
+//Basic Arithmetic Problems
+
+// Q1.In one night, a movie theater sells tickets for 6450 dollars.Each ticket costs 15 dollars.How many tickets were sold ?
+
+let sum = 6450;
+let ticket = 15;
+
+console.log("This amount of tickets was sold:", sum / ticket);
+
+//Q2.Sylvia's income is 500 dollars per week. How much does Sylvia makes every year?
+
+let inc = 500;
+
+console.log("Silvia has a yearly income of:", inc * 52);
+
+//Percentage
+
+// Q3.Calculate the percentage of 17 / 30. Expected output: number %
+
+let w = 17;
+let g = 30;
+
+console.log((w / g) * 100, "%");
+
+//Geometry Formulas
+
+//Q4.Calculate the perimeter of a square.Assume each side is 4.75cm. U=a+b+c+d
+
+let side = 4.75;
+
+console.log("The perimeter of the square is ", side + side + side + side);
+
+//Q5.Calculate the perimeter of a triangle.Assume the length of the sides are 5cm, 6cm, 7cm.
+
+let a = 5;
+let b = 6;
+let c = 7;
+
+console.log("The perimeter of the triangle is ", a + b + c);
+
+//Q6.Calculate the area of a square.Each side is 5cm.
+let d = 5;
+
+console.log("The area are:", 5 * 5, "square cm's");
