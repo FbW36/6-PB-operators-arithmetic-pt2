77fc01ae4e200567f6dd90296f2048f82bc3f79a
Tomas <eliz.tomas23@gmail.com>
2020-10-06 15:21:36 +0200

    math problems solved

---
+++ tomas/index.js
@@ -0,0 +1,32 @@
+// Q1
+let profit = 6450
+let ticketPrice = 15
+let amount = profit / ticketPrice
+console.log('They sold ' + amount + ' tickets.')
+
+// Q2
+let income = 500
+let weeks = 52
+let yearIncome = income * weeks
+console.log('Sylvia makes ' + yearIncome + '$ each year.')
+
+//Q3
+let output = 17/30*100
+console.log(output + '%');
+
+//Q4
+let sideLength = 4.75
+let perimeter = 4 * 4.75
+console.log(perimeter + ' cm')
+
+// Q5
+let a = 6
+let b = 7
+let c = 8
+let trianglePerimeter = a + b + c
+console.log(trianglePerimeter + " cm")
+
+//Q6
+let squareSide = 5
+let area = squareSide * squareSide
+console.log(area + ' cm2')
