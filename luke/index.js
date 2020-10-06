d36cb3ed068645d4285b1ffa36dcb4bc2446635d
Luke Dowling <lpdowling26@googlemail.com>
2020-10-06 13:56:26 +0200

    completed

---
+++ luke/index.js
@@ -0,0 +1,23 @@
+// Q1.In one night, a movie theater sells tickets for 6450 dollars.Each ticket costs 15 dollars.How many tickets were sold ?
+let tiketsSold = 6450 / 15;
+console.log(tiketsSold);
+
+// Q2.Sylvia's income is 500 dollars per week. How much does Sylvia makes every year?
+let yearIncome = 500 * 12;
+console.log("$" + yearIncome);
+
+// Q3.Calculate the percentage of 17 / 30. Expected output: number %
+let percentage = (17 / 30) * 100;
+console.log(percentage + "%");
+
+// Q4.Calculate the perimeter of a square.Assume each side is 4.75cm.
+let squarePerimeter = 4 * 4.75;
+console.log(squarePerimeter + "cm");
+
+// Q5.Calculate the perimeter of a triangle.Assume the length of the sides are 5cm, 6cm, 7cm.
+let trianglePerimeter = 5 + 6 + 7;
+console.log(trianglePerimeter + "cm");
+
+// Q6.Calculate the area of a square.Each side is 5cm.
+let squareArea = Math.pow(5, 2);
+console.log(squareArea + "cm");
