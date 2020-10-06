c404ef97c080a0f454d0b212ef3a7421ad4956c9
Muhasin <mahmudmuhasin@gmail.com>
2020-10-06 17:38:24 +0200

    exercise_6

---
+++ Muhasin/index.js
@@ -0,0 +1,35 @@
+// Q1.In one night, a movie theater sells tickets for 6450 dollars. Each ticket costs 15 dollars. How many tickets were sold?
+
+let perticket = 15;
+let totalprice = 6450;
+let totalticket = totalprice / perticket;
+console.log(totalticket);
+
+// Q2. Sylvia's income is 500 dollars per week. How much does Sylvia makes every year?
+let perweeek = 500;
+let year = 52;
+let income = (year * perweeek);
+console.log(income);
+
+// Q3. Calculate the percentage of 17/30. Expected output: number%
+let a = 17;
+let b = 30;
+let percentage = (17 / 30) * 100;
+console.log(percentage + '%');
+
+// Q4. Calculate the perimeter of a square. Assume each side is 4.75cm.
+let eachSide = 4.75;
+let perimeter = (eachSide * 4);
+console.log(perimeter);
+
+// Q5. Calculate the perimeter of a triangle. Assume the length of the sides are 5cm, 6cm, 7cm.
+let sideA = 5;
+let sideB = 6;
+let sideC = 7;
+let triangle = (sideA + sideB + sideC);
+console.log(triangle);
+
+//Q6. Calculate the area of a square. Each side is 5cm.
+let sideLength = 5;
+let square = (sideLength * 4);
+console.log(square);
