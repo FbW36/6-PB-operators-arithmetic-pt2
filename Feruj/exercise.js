16f875bf44896f7fb5e1303506bbbded1e6d4237
Feruj Ahammed <ferujahammed@gmail.com>
2020-10-06 15:43:45 +0200

    initial commit

---
+++ Feruj/exercise.js
@@ -0,0 +1,24 @@
+let totalSell = 6450;
+let ticket = 15;
+let totalTicket = totalSell / ticket;
+console.log("Number of ticket sold: $", totalTicket);
+//
+let oneWeek = 500;
+let yearWeek = 52;
+let totalIncome = oneWeek * yearWeek;
+console.log("Yearly income : $", totalIncome);
+// percentage
+let percentage = (17 / 30) * 100;
+console.log("17 makes", Math.round(percentage), "% of 30.");
+//
+let prem = 4.75;
+let sqPrem = prem * 4;
+console.log(sqPrem + "cm");
+//
+
+let trianglePerimeter = 5 + 6 + 7;
+console.log(trianglePerimeter + "cm");
+//
+let squre = 5;
+let squareArea = squre * squre;
+console.log(squareArea + "cm");
