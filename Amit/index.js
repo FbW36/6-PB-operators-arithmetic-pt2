07479581bfff7bf7c9442ab9fd4b12c060048423
Amit833 <amitmajumder833@gmail.com>
2020-10-06 15:59:44 +0200

    first commit

---
+++ Amit/index.js
@@ -0,0 +1,37 @@
+// q 1
+let sells = 6450;
+let cost = 15;
+let sold = sells / cost;
+console.log("Tickes sold", sold); // 430
+
+// console.log(`${sold} bought tickets`);
+
+// 2
+let doller = 500;
+let week = 52;
+let mul = doller * week;
+console.log(mul);
+
+// 3
+let a = 17;
+let b = 30;
+let result = (17 / 30) * 100;
+
+console.log(result);
+
+//4
+let weidth = 4.75;
+let square = weidth * 4; // weidth+weadth+weidth+weidth
+console.log(square);
+
+//5
+let one = 5;
+let two = 6;
+let three = 7;
+perimeter = one * two * three;
+console.log(perimeter);
+
+//6 area
+side = 5;
+squ = side * side;
+console.log(squ);
