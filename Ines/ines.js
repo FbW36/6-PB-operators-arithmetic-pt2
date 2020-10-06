1f5295ba866a608182dbeb4b420eed3a13258fcc
Ines <inesfcnogueira@gmail.com>
2020-10-06 15:44:24 +0200

    FC Ines

---
+++ Ines/ines.js
@@ -0,0 +1,25 @@
+// Q1. In one night, a movie theater sells tickets for 6450 dollars. Each ticket costs 15 dollars. How many tickets were sold?
+let movieTickets = 6450;
+let ticketCosts = 15;
+console.log(movieTickets / ticketCosts + " movie theater tickets were sold");
+
+// Q2. Sylvia's income is 500 dollars per week. How much does Sylvia makes every year?
+let weeks = 4 * 12;
+let income = 500;
+console.log("Sylvia makes " + weeks * income + " per year ");
+
+// Q3. Calculate the percentage of 17/30. Expected output: number%
+let percentage = (17 / 30) * 100;
+console.log(percentage + "%");
+
+// Q4. Calculate the perimeter of a square. Assume each side is 4.75cm.
+let perimeterSquare = 4.75 * 4;
+console.log("The square is " + perimeterSquare + " cm");
+
+// Q5. Calculate the perimeter of a triangle. Assume the length of the sides are 5cm, 6cm, 7cm.
+let perimeterTriangle = 5 + 6 + 7;
+console.log("The triangle is " + perimeterTriangle + " cm");
+
+// Q6. Calculate the area of a square. Each side is 5cm.
+let area = 5 * 5;
+console.log("The square has an area of: " + area + " cm²");
