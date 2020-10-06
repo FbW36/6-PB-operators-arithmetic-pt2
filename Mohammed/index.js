d5ec035aa3bccf7d675bb4c5a4e56bdd82f1a12b
mashraee <m.almashraee@gmail.com>
2020-10-06 15:38:39 +0200

    exercise -6

---
+++ Mohammed/index.js
@@ -0,0 +1,35 @@
+
+// Q1.
+let ticketCost = 15;
+let priceOfAllTickets = 6450;
+let totalNumberOfTickets = priceOfAllTickets / ticketCost;
+console.log("There are " + totalNumberOfTickets + " tickets sold" );
+
+// Q2.
+let weeklyIncome = 500;
+let numberOfDaysPerYear = 356;
+let numberOfWeeksPerYear = numberOfDaysPerYear/7;
+let totalIncomePerYear = numberOfWeeksPerYear * weeklyIncome;
+console.log("The total income per year is " + totalIncomePerYear);
+
+// Q3.
+let firstNumber = 17;
+let secondNumber = 30;
+console.log("The percentage of the numbers 17/30 is: " + (17/30)*100);
+
+// Q4.
+let squareEdge = 4.75;
+perimeterOfSquare = 4 * squareEdge;
+console.log("The perimeter of a square is: " + perimeterOfSquare);
+
+// Q5.
+let lengthOfFirstEdge = 5;
+let lengthOfSecondEdge = 6;
+let lengthOfThirdEdge = 7;
+let perimeterOfTriangle = lengthOfFirstEdge + lengthOfSecondEdge + lengthOfThirdEdge;
+console.log("The Perimeter of the Triangle is: " + perimeterOfTriangle);
+
+// Q6. 
+let squareSide = 5;
+let areaOfSquare = squareSide * squareSide;
+console.log("The Area of a Square is: " + areaOfSquare);
