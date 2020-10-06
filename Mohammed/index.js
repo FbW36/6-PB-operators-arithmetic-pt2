e86e2f460f7b1b48d16a3f0e6bba4187f5965468
mashraee <m.almashraee@gmail.com>
2020-10-06 16:11:26 +0200

    exercise -6

--- Mohammed/index.js
+++ Mohammed/index.js
@@ -3,33 +3,33 @@
 let ticketCost = 15;
 let priceOfAllTickets = 6450;
 let totalNumberOfTickets = priceOfAllTickets / ticketCost;
-console.log("There are " + totalNumberOfTickets + " tickets sold" );
+console.log("Q1. There are " + totalNumberOfTickets + " tickets sold" );
 
 // Q2.
 let weeklyIncome = 500;
 let numberOfDaysPerYear = 356;
 let numberOfWeeksPerYear = numberOfDaysPerYear/7;
 let totalIncomePerYear = numberOfWeeksPerYear * weeklyIncome;
-console.log("The total income per year is " + totalIncomePerYear);
+console.log("Q2. The total income per year is " + totalIncomePerYear);
 
 // Q3.
 let firstNumber = 17;
 let secondNumber = 30;
-console.log("The percentage of the numbers 17/30 is: " + (17/30)*100);
+console.log("Q3. The percentage of the numbers 17/30 is: " + (17/30)*100 + "%");
 
 // Q4.
 let squareEdge = 4.75;
 perimeterOfSquare = 4 * squareEdge;
-console.log("The perimeter of a square is: " + perimeterOfSquare);
+console.log("Q4. The perimeter of a square is: " + perimeterOfSquare);
 
 // Q5.
 let lengthOfFirstEdge = 5;
 let lengthOfSecondEdge = 6;
 let lengthOfThirdEdge = 7;
 let perimeterOfTriangle = lengthOfFirstEdge + lengthOfSecondEdge + lengthOfThirdEdge;
-console.log("The Perimeter of the Triangle is: " + perimeterOfTriangle);
+console.log("Q5. The Perimeter of the Triangle is: " + perimeterOfTriangle);
 
 // Q6. 
 let squareSide = 5;
 let areaOfSquare = squareSide * squareSide;
-console.log("The Area of a Square is: " + areaOfSquare);
+console.log(" Q6. The Area of a Square is: " + areaOfSquare);
