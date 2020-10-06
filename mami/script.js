ea8e58aca28503b70d9d3515cfb87f74ffaa9b94
carveler <3heyaberlin@gmail.com>
2020-10-06 15:10:35 +0200

    mami

---
+++ mami/script.js
@@ -0,0 +1,17 @@
+let x = 6450 / 15;
+console.log(x);
+
+let y = 500 * 4 * 12;
+console.log(y + "$");
+
+let z = 17 / 30;
+console.log(z + "%");
+
+let a = 4.75 * 4;
+console.log(a + "cm");
+
+let b = 5 + 6 + 7;
+console.log(b + "cm");
+
+let c = 5 * 5;
+console.log(c + "㎠");
